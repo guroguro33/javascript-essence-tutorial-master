1fdd9bf2dd961bcba61d58f8b100c8d83199b361
toru <t02f447f@gmail.com>
2021-02-24 05:59:22 +0900

    IMPROVE JS_memo.md

---
+++ 030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
@@ -0,0 +1,6 @@
+let a = 0;
+var b = 0;
+function c() { };
+
+console.log(b);
+console.log(window.b);
---
+++ 030_スコープ/020_関数スコープとブロックスコープ/start/main.js
@@ -0,0 +1,8 @@
+{
+  var c = 1;
+  var d = function () {
+    console.log('d is called');
+  }
+
+}
+d();
