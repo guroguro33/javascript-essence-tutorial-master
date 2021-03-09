2e13bea9e1bab82375ba04ac203eb79f428bafc2
toru <t02f447f@gmail.com>
2021-03-10 06:01:15 +0900

    参照と分割代入から終了確認1つ目まで

--- 040_変数/910_修了確認_falsyとtruthy/start/main.js
+++ 040_変数/910_修了確認_falsyとtruthy/start/main.js
@@ -2,15 +2,25 @@
  * 問題１：
  * 以下のコンソールにはどのような値が表示されるでしょうか？
  */
-// console.log("0 == false", 0 == false);
-// console.log("0 === false", 0 === false);
-// console.log('"false" == false', "false" == false);
-// console.log('"0" == 0', "0" == 0);
-// console.log('Boolean("0") === false', Boolean("0") === false);
-// console.log('Boolean(0) === false', Boolean(0) === false);
-// console.log('!Boolean(0) === false', !Boolean(0) === false);
-// console.log('-1 == false', -1 == false);
-// console.log('!10 === false', !10 === false);
+console.log("0 == false", 0 == false);
+console.log("0 === false", 0 === false);
+console.log('"false" == false', "false" == false);
+console.log('"0" == 0', "0" == 0);
+console.log('Boolean("0") === false', Boolean("0") === false);
+console.log('Boolean(0) === false', Boolean(0) === false);
+console.log('!Boolean(0) === false', !Boolean(0) === false);
+console.log('-1 == false', -1 == false);
+console.log('!10 === false', !10 === false);
+
+// 0 == false, true
+// 0 === false, false
+// "false" == false, false
+// "0" == 0, true
+// Boolean("0") === false, false
+// Boolean(0) === false, true
+// !Boolean(0) === false, false
+// -1 == false, false
+// !10 == false, true 
 
 /**
  * 問題２：
@@ -20,13 +30,16 @@
  * 以下の関数fnの初期化を適切に書き直してください。
  * ※aには0以上の整数値が渡ってくるものとします。
  */
-let a = 1;
+let a = null;
 
-function fn(num) {
-    num = num || -1;
+function fn(num = -1) {
+  if (num === undefined || num === null) {
+    num = -1;
+  }
     console.log(num);
 }
 fn(a);
+// fn(null)
 
 
 /**
@@ -39,3 +52,13 @@ fn(a);
  * greeting("Bob"); -> 出力結果："hello, Bob"
  *
  */
+
+function greeting(name, greet) {
+  let message = greet || "hello";
+
+  // console.log(message + ", " + name);
+  console.log(`${greet}, ${name}`);
+}
+ 
+greeting("Bob", "hi");
+greeting("Bob");
