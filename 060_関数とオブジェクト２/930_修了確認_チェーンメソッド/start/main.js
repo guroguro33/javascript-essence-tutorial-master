cba7e2260e219800b497f36d76c7c1dc50abb2b1
toru <t02f447f@gmail.com>
2021-04-03 08:00:05 +0900

    ADD 終了確認_チェーンメソッド

--- 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
+++ 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
@@ -20,16 +20,64 @@
  * calc.set(10)
  * 	.minus()
  * 	.set(3) -> '7'を出力
- * 	.mutiply()
+ * 	.multiply()
  * 	.set(6) -> '42'を出力（10 - 3) * 6
  */
+
+class Calculator {
+  constructor() {
+    this.result = null;
+    this.calc = '';
+  }
+
+  set(val) {
+    switch (this.calc) {
+      case 'plus':
+        this.result = this.result + val;
+        console.log(this.result);
+        break;
+      case 'minus':
+        this.result = this.result - val;
+        console.log(this.result);
+        break;
+      case 'multiply':
+        this.result = this.result * val;
+        console.log(this.result);
+        break;
+      case 'divide':
+        this.result = this.result / val;
+        console.log(this.result);
+        break;
+      default:
+        this.result = val;
+        break;
+    }
+    return this;
+  }
+  plus() {
+    this.calc = 'plus';
+    return this;
+  }
+  minus() {
+    this.calc = 'minus';
+    return this;
+  }
+  multiply() {
+    this.calc = 'multiply';
+    return this;
+  }
+  divide() {
+    this.calc = 'divide';
+    return this;
+  }
+}
 
 const calc = new Calculator();
 
 calc.set(10)
 	.minus()
 	.set(3)
-	.mutiply()
+	.multiply()
 	.set(6)
 	.divide()
 	.set(2)
