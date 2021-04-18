29aa3b6c85d0c5459a29266bade259fa25ce720b
toru <t02f447f@gmail.com>
2021-04-19 05:37:57 +0900

    ADD Promiseの並列処理

--- 080_非同期処理/070_Promiseと並列処理/start/main.js
+++ 080_非同期処理/070_Promiseと並列処理/start/main.js
@@ -1,16 +1,38 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
+  return new Promise(function(resolve,reject) {
     setTimeout(function() {
       console.log(val++);
-      resolve(val);
-    }, 1000);
+      reject(val);
+    }, val * 500);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+// 並列処理する場合はPromise.all(繰り返し処理するもの)を使う
+allの中にはpromiseのインスタンスで反復可能なものを入れる
+Promise.all([sleep(2), sleep(3), sleep(4)])
+  .then(function (data) {
+    console.log(data); // 2, 3, 4 のあとに [3, 4, 5]と出力
+  })
+
+// Promise.raceを使うと、配列の最初のresolveが帰った直後にthenメソッドを実行する 
+Promise.race([sleep(2), sleep(3), sleep(4)])
+  .then(function (data) {
+    console.log(data); // 2, 3, 3, 4と出力
+  })
+
+// Promise.allSettledを使うと、rejectの場合にもcatchにいかず、thenが実行される
+// 返ってくる引数もオブジェクトであり、resolveだとstatusはfulfilled、rejectだとstatusはrejected
+Promise.allSettled([sleep(2), sleep(3), sleep(4)])
+  .then(function (data) {
+    console.log(data); // 2, 3, 4 のあとにオブジェクトが出力
+  }).catch(function (e) {
+    console.error(data); // allSettledの場合はrejectでもcatchされない
+  })
+
+// sleep(0).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
--- JS_memo.md
+++ JS_memo.md
@@ -1004,3 +1004,41 @@ sleep(0) // 出力0
     return sleep(val); // 出力5
   });
 ```
+
+## 並列処理
+
+- 並列処理する場合は Promise.all(繰り返し処理する引数)を使う
+- Promise.race を使うと、配列の最初の resolve が帰った直後に then メソッドを実行する
+- 新しい Promise.allSettled を使うと、reject の場合にも catch にいかず、then が実行される
+
+```javascript
+function sleep(val) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log(val++);
+      reject(val);
+    }, val * 500);
+  });
+}
+
+// 並列処理する場合はPromise.all(繰り返し処理するもの)を使う
+allの中にはpromiseのインスタンスで反復可能なものを入れる;
+Promise.all([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+  console.log(data); // 2, 3, 4 のあとに [3, 4, 5]と出力
+});
+
+// Promise.raceを使うと、配列の最初のresolveが帰った直後にthenメソッドを実行する
+Promise.race([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+  console.log(data); // 2, 3, 3, 4と出力
+});
+
+// Promise.allSettledを使うと、rejectの場合にもcatchにいかず、thenが実行される
+// 返ってくる引数もオブジェクトであり、resolveだとstatusはfulfilled、rejectだとstatusはrejected
+Promise.allSettled([sleep(2), sleep(3), sleep(4)])
+  .then(function (data) {
+    console.log(data); // 2, 3, 4 のあとにオブジェクトが出力
+  })
+  .catch(function (e) {
+    console.error(data); // allSettledの場合はrejectでもcatchされない
+  });
+```
