dbd6b9791d5306c8e094c20c8f9216430f33f046
toru <t02f447f@gmail.com>
2021-04-23 05:34:03 +0900

    ADD 例外処理

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,11 +1,37 @@
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  if (response.ok) {
+    const json = await response.json();
+    if (!json.length) {
+      throw new NoDataError('no data found');
+    }
+    return json;
+  }
+}
 
+// カスタムエラーを作成
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NoDataError';
+  }
+}
 
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
-// }
-
-// fetchUsers();
+async function init() {
+  try {
+    const json = await fetchUsers();
+    for(const user of json) {
+      console.log(`I'm ${user.name}, ${user.age} years old`)
+    }
+  } catch (e) {
+    // カスタムエラーを使ってハンドリングできる
+    if (e instanceof NoDataError) {
+      console.error(e);
+    } else {
+      console.error('Oops, something went wrong');
+    }
+  } finally {
+    console.log('bye');
+  }
+}
+init();
--- JS_memo.md
+++ JS_memo.md
@@ -1140,3 +1140,50 @@ async function fetchUsers() {
 // 実行
 fetchUsers();
 ```
+
+### 例外処理とエラー
+
+- try-catch-finally でハンドリング可能
+
+```javascript
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  // response.okがtrueの時に処理を継続
+  if (response.ok) {
+    const json = await response.json();
+    // データが取れなかった場合にエラーを投げる
+    if (!json.length) {
+      // カスタムエラーに投げる
+      throw new NoDataError('no data found');
+    }
+    return json;
+  }
+}
+
+// カスタムエラーを作成
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NoDataError';
+  }
+}
+
+async function init() {
+  try {
+    const json = await fetchUsers();
+    for (const user of json) {
+      console.log(`I'm ${user.name}, ${user.age} years old`);
+    }
+  } catch (e) {
+    // カスタムエラーを使ってハンドリングできる
+    if (e instanceof NoDataError) {
+      console.error(e);
+    } else {
+      console.error('Oops, something went wrong');
+    }
+  } finally {
+    console.log('bye');
+  }
+}
+init();
+```
