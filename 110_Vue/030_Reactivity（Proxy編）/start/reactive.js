c0c917a0b7bd80ae1641405a91a65f1dadf38e48
toru <t02f447f@gmail.com>
2021-05-16 07:30:29 +0900

    ADD Reactivity続き

--- 110_Vue/030_Reactivity（Proxy編）/start/reactive.js
+++ 110_Vue/030_Reactivity（Proxy編）/start/reactive.js
@@ -1,3 +1,21 @@
+const handler = {
+  get(target, prop, receiver) {
+    const result = Reflect.get(target, prop, receiver);
+    console.log('%c[reactive:get]', 'background: green;color: white;', prop, result)
+    return result;
+  },
+  set(target, prop, value, receiver) {
+    const result = Reflect.set(target, prop, value, receiver);
+    console.log('%c[reactive:set]', 'background: red;color: white;', prop, result)
+    trigger();
+    return result;
+  }
+}
+
+function reactive(target) {
+  return new Proxy(target, handler);
+}
+
 let activeEffect = null;
 function effect(fn) {
   activeEffect = fn;
@@ -5,4 +23,4 @@ function effect(fn) {
 function trigger() {
   activeEffect();
 }
-export { effect, trigger };
+export { effect, trigger,reactive };
