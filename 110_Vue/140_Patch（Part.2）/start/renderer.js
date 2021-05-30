2c809aec1566b8aef1af6bd6d5e3bdbb7ae2ff75
toru <t02f447f@gmail.com>
2021-05-30 14:09:59 +0900

    ADD patchその2

--- 110_Vue/140_Patch（Part.2）/start/renderer.js
+++ 110_Vue/140_Patch（Part.2）/start/renderer.js
@@ -11,11 +11,31 @@ function createVNode(type = '', props = {}, children = '') {
 function patch(n1, n2, container) {
   console.log(n1, n2);
   let el;
-  if(n1.type !== n2.type) {
+    if (n1.type !== n2.type) {
+    // divを生成し、bodyの中に配置 
     el = nodeOps.create(n2.type);
     nodeOps.append(container, el);
   }
-  if(n1.children !== n2.children) {
+    for (const key in n2.props) {
+        const prevProp = n1.props[key];
+        const nextProp = n2.props[key];
+        // console.log(prevProp, nextProp);
+
+        if (prevProp !== nextProp) {
+            // propが一致しなければ、属性を追加
+            if (key.startsWith('on')) {
+                console.log(key.substring(2).toLowerCase(), nextProp);
+                nodeOps.on(el, key.substring(2).toLowerCase(), () => {
+                    nextProp();
+                })
+            } else {
+                nodeOps.setAttr(el, key, nextProp);
+            }
+        }
+  }
+    // console.log(n2.props)
+  if (n1.children !== n2.children) {
+    // 子Nodeの中身が一致しなければ、innerHTMLで追加   
     nodeOps.html(el, n2.children);
   }
 }
